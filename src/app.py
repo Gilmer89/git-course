67d530c62c61a75c9c5a381d75d75e7c06287d9b
gilmer <gilmerperez4@gmail.com>
2022-07-05 22:28:26 -0500

    Fix: colucionado el error de las funciones de saludo y raiz. BC

--- src/app.py
+++ src/app.py
@@ -4,7 +4,11 @@ app = Flask( __name__)
 
 @app.route('/')
 def index():
-    return "Bye Alfonso!"
+    return "Bye code!"
+
+@app.route('/hello')
+def greating():
+    return "Hello World!"
 
 @app.route('/sum/<int:a>/<int:b>')
 def sum(a: int, b: int):
